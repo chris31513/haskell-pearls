b4620b7579a3aeb32946b5ec120c8147ffa7ba77
Christian <chris3151@ciencias.unam.mx>
2020-02-02 15:24:15 -0600

    mergeSort con compare list

--- mergeSort.hs
+++ mergeSort.hs
@@ -14,3 +14,13 @@ module MergeSort where
     mezcla [] xs = xs
     mezcla xs [] = xs
     mezcla (x:xs) (y:ys) = if x > y then y:(mezcla (x:xs) ys) else x:(mezcla xs (y:ys))
+
+    mezclaCon :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+    mezclaCon f xs [] = xs
+    mezclaCon f [] xs = xs
+    mezclaCon f (x:xs) (y:ys) = if (f x y) == LT then x:(mezclaCon f xs (y:ys)) else y:(mezclaCon f (x:xs) ys)
+
+    mergeSortCon :: (a -> a -> Ordering) -> [a] -> [a]
+    mergeSortCon f [] = []
+    mergeSortCon f [x] = [x]
+    mergeSortCon f xs = mezclaCon f (mergeSortCon f s) (mergeSortCon f r) where (s, r) = parte xs

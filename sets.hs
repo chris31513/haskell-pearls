931441d9d7ddd0fb10fd15e267f57a5d5797e4f8
Christian Rodrigo Garcia <chrisrg@ciencias.unam.mx>
2020-02-24 17:51:21 -0600

    Avances tarea 2

---
+++ sets.hs
@@ -0,0 +1,21 @@
+module Sets where
+
+  gtrPower2 :: Int -> Int
+  gtrPower2 0 = 0
+  gtrPower2 n = listLast [y | y <- [0..(n-1)], isPowerOfTwo y]
+
+  listLast :: [a] -> a
+  listLast [x] = x
+  listLast (_:xs) = listLast xs
+  listLast [] = error "Can't do last of an empty list!"
+
+  isPowerOfTwo :: Int -> Bool
+  isPowerOfTwo n 
+    | 0 <- n    = True
+    | 2 <- n    = True
+    | n `mod` 2 == 0  = isPowerOfTwo (n `div` 2)
+    | otherwise   = False
+
+
+  inarow :: (Eq a) => [a] -> Int
+  inarow (x:xs) = if (elem x xs) then 1 + (inarow) else 0
